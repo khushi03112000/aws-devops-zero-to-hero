daab3872b69f5c9024cbd06c2d6346d9f6c99370
khushi03112000 <khushimail03112000@gmail.com>
2025-03-19 19:08:07 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull abhishekf5/simple-python-flask-app
+docker pull khushi0311/simple-python-app:tagname
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app
+docker run -d -p 5000:5000 khushi0311/simple-python-flask-app
